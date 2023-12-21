94f6610b2c032795e82e411ac7184de7a81e4c09
gregorywmorris <gwm1980@gmail.com>
2023-12-21 06:15:59 -0500

    OOP

--- Python/OOP/main.py
+++ Python/OOP/main.py
@@ -1,6 +1,10 @@
 class Item: 
+    # Class attribute
+    discount_rate = 0.8
+
     def __init__(self,name:str, price:float, quantiy=0):
-        # run validations
+
+        # Run validations
         assert price >= 0, f"Price {price} is not greater than 0"
         assert quantiy >= 0, f"Quantity {quantiy} is not greater than 0"
 
@@ -13,11 +17,19 @@ class Item:
 
     def caculate_total_price(self):
         return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * self.discount_rate
 
-item1 = Item("Phone",100,5)
+
+
+item1 = Item("Phone",100,10)
+item1.apply_discount()
 print(item1.caculate_total_price())
 
-item2 = Item("Laptop",1000,5)
+item2 = Item("Laptop",1000,10)
+item2.discount_rate = 0.7
+item2.apply_discount()
 print(item2.caculate_total_price())
 
 item3 = Item("PC",2000)
