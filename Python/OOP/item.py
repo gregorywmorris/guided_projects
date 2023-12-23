f8c6e3b70b9a52e46889b4aec6d19411a2495a5e
gregorywmorris <gwm1980@gmail.com>
2023-12-23 09:20:13 -0500

    changed main to item, updated code

--- Python/OOP/item.py
+++ Python/OOP/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item: 
     # Class attribute
     discount_rate = 0.8
@@ -24,15 +26,41 @@ class Item:
     
     def apply_discount(self):
         self.price = self.price * self.discount_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv','r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+        
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity')),
+            )
+
+    @staticmethod
+    def is_integer(num):
+        if isinstance(num, float):
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
     
     def __repr__(self) -> str:
         return f"Item('{self.name}',{self.price},{self.quantity})"
 
-
-item1 = Item("Phone", 100, 1)
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouse", 50, 5)
-item5 = Item("Keyboard", 75, 5)
+Item.instantiate_from_csv
 
 print(Item.all)
+
+
+# item1 = Item("Phone", 100, 1)
+# item2 = Item("Laptop", 1000, 3)
+# item3 = Item("Cable", 10, 5)
+# item4 = Item("Mouse", 50, 5)
+# item5 = Item("Keyboard", 75, 5)
+
