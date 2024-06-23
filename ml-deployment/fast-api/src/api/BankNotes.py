86f96759a539412d23031eaa9538947ceb9d34dd
gregorywmorris <gwm1980@gmail.com>
2024-06-23 14:39:11 -0400

    cleaned up code to be more pythonic

--- ml-deployment/fast-api/src/api/BankNotes.py
+++ ml-deployment/fast-api/src/api/BankNotes.py
@@ -6,6 +6,7 @@ Created on 6/23/2024
 from pydantic import BaseModel
 # 2. Class which describes Bank Notes measurements
 class BankNote(BaseModel):
+    """Class which describes Bank Notes measurements"""
     variance: float
     skewness: float
     curtosis: float
